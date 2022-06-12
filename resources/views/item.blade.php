390f7fa46a60b993eab1eff0cc1bf73a97a06b90
Freskim Veliu <freskim.veliu@tenton.co>
2022-06-12 16:26:34 +0200

    improve item style

--- resources/views/item.blade.php
+++ resources/views/item.blade.php
@@ -1,5 +1,5 @@
-<li class="cursor-default select-none px-4 py-2">
-    <a href="#" @click.prevent="goTo(item.id, result.class)">
-        <span x-text="item.name"></span>
-    </a>
+<li
+    @click.prevent="goTo(item.id, result.class)"
+    class="cursor-pointer px-4 py-2 hover:bg-gray-50">
+    <span x-text="item.name"></span>
 </li>
